d2e2f094d2ed111f2497faefa61f825c61636bf1
Elton Machado Vieira <33964353+Conveniencia-Digital@users.noreply.github.com>
2023-01-03 22:07:51 -0400

    create user

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path, include
+from usuarios.views import SignUp
 
 
 urlpatterns = [
     path('accounts/', include('django.contrib.auth.urls')),
+    path('register/', SignUp.as_view(), name='register')
     
 ]
---
+++ usuarios/views.py
@@ -0,0 +1,8 @@
+from django.views.generic.edit import CreateView
+from django.urls import reverse_lazy
+from usuarios.forms import SignUpForm
+
+class SignUp(CreateView):
+    template_name = 'registration/sign-up.html'
+    form_class = SignUpForm
+    success_url = reverse_lazy('login')
