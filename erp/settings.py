e416516e9e7dbf7c26fe7a8b4a98997134f4401d
Elton Machado Vieira <33964353+Conveniencia-Digital@users.noreply.github.com>
2023-06-02 17:40:21 -0400

    .

--- erp/settings.py
+++ erp/settings.py
@@ -108,7 +108,7 @@ ROOT_URLCONF = 'erp.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
