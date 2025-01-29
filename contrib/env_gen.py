69c45a7aaf0298b2acd433e9cccf8e6de9f362b0
rg3915 <regis42santos@gmail.com>
2025-01-29 05:05:46 -0300

    Atualiza contrib/env_gen.py. close #8

--- contrib/env_gen.py
+++ contrib/env_gen.py
@@ -16,19 +16,23 @@ DEBUG=True
 SECRET_KEY=%s
 ALLOWED_HOSTS=127.0.0.1,.localhost,0.0.0.0
 
+#CSRF_TRUSTED_ORIGINS=
+
 #DATABASE_URL=postgres://USER:PASSWORD@HOST:PORT/NAME
-#POSTGRES_DB=
-#POSTGRES_USER=
-#POSTGRES_PASSWORD=%s
-#DB_HOST=localhost
+
+#DB_NAME=
+#DB_USER=postgres
+#DB_PASSWORD=
+#DB_HOST=
+#DB_PORT=5528
 
 #DEFAULT_FROM_EMAIL=
 #EMAIL_BACKEND=django.core.mail.backends.smtp.EmailBackend
-#EMAIL_HOST=localhost
-#EMAIL_PORT=
+#EMAIL_HOST=smtp.gmail.com
+#EMAIL_PORT=587
+#EMAIL_USE_TLS=True
 #EMAIL_HOST_USER=
 #EMAIL_HOST_PASSWORD=
-#EMAIL_USE_TLS=True
 """.strip() % (secret_key, password)
 
 # Writing our configuration file to '.env'
