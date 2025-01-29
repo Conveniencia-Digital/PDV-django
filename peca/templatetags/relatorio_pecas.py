55edb2494a1d52b9e73a88bb1eacbded6229ae26
rg3915 <regis42santos@gmail.com>
2025-01-29 05:47:41 -0300

    refactor: Refatora os métodos de templatetags/relatorio_pecas.py. close #10

--- peca/templatetags/relatorio_pecas.py
+++ peca/templatetags/relatorio_pecas.py
@@ -1,44 +1,33 @@
 from django import template
-from django.db.models.aggregates import Sum
+from django.db.models import Sum
 
 from peca.models import Pecas
 
 register = template.Library()
 
 
-@register.simple_tag
-def preco_venda(self):
-    i = 0
-    total = Pecas.objects.filter(usuario=self.request.user).aggregate(total=Sum('preco_peca'))
- 
-    for i in total.values():
-        if i == None:
-            return(0.00)
-    return i
+@register.simple_tag(takes_context=True)
+def preco_venda(context):
+    user = context['request'].user
+    total = Pecas.objects.filter(usuario=user).aggregate(total=Sum('preco_peca'))['total']
+    return total or 0.00
 
 
-@register.simple_tag
-def preco_custo(self):
-    i = 0
-    tot_custo = Pecas.objects.filter(usuario=self.request.user).aggregate(tot_custo=Sum('preco_de_custo'))
-    for i in tot_custo.values():
-        if i == None:
-            return(0.00)
-    return i
+@register.simple_tag(takes_context=True)
+def preco_custo(context):
+    user = context['request'].user
+    total = Pecas.objects.filter(usuario=user).aggregate(total=Sum('preco_de_custo'))['total']
+    return total or 0.00
 
 
-@register.simple_tag
-def total_peca(self):
-    tot = Pecas.objects.filter(usuario=self.request.user).count()
-    if tot == None:
-        return 0
-    else:
-        return tot
-        
-@register.simple_tag
-def calculo():
-    calc = preco_venda() - preco_custo()
-    return calc 
+@register.simple_tag(takes_context=True)
+def total_peca(context):
+    user = context['request'].user
+    return Pecas.objects.filter(usuario=user).count()
 
 
-
+@register.simple_tag(takes_context=True)
+def calculo(context):
+    venda = preco_venda(context)
+    custo = preco_custo(context)
+    return venda - custo
